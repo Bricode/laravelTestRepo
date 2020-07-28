14dc7c5cc91f4bafe4a08fca02cf2f4c90593785
bricode <bcharm1100@gmail.com>
2020-07-28 14:09:39 +1200

    Initial commit

---
+++ TestLaravelApp/app/Http/Controllers/PostsController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use DB;
+use App\Post;
+
+class PostsController
+{
+    public function show($slug){
+        #$post = \DB::table('laravel')->where('slug', $slug)->first();
+        $post = post::where('slug', $slug)->firstOrFail();        
+
+        return view('post', [
+            'post' => $post
+        ]);
+    }
+}
